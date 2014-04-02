8367aa3bc6affcb366f31b07eb06494d64a02ce5
Michael Sparks <mike@sparksm.com>
2014-04-02 17:54:56 -0400

    first test

--- jquery.scroll-intention.js
+++ jquery.scroll-intention.js
@@ -1,4 +1,8 @@
 /*global module:true*/
+
+// mousewheel event hooks borrowed from
+// https://github.com/brandonaaron/jquery-mousewheel/
+
 (function (factory) {
 	if (typeof define === 'function' && define.amd) {
 
@@ -37,15 +41,25 @@
 		return average / count;
 	};
 
+	var toFix  = ['wheel', 'mousewheel', 'DOMMouseScroll', 'MozMousePixelScroll'];
+	var toBind = ('onwheel' in document || document.documentMode >= 9) ? ['wheel'] : ['mousewheel', 'DomMouseScroll', 'MozMousePixelScroll'];
+
+	if ($.event.fixHooks) {
+		for (var l = toFix.length; l;) {
+			$.event.fixHooks[toFix[--l]] = $.event.mouseHooks;
+		}
+	}
+	
 	$.event.special.mousewheelintention = {
 		version: '0.1.0',
 	
 		setup: function () {
-			if (window.addEventListener) {
-				window.addEventListener('mousewheel', mouseWheelListener, false);
-				window.addEventListener('DOMMouseScroll', mouseWheelListener, false);
-			} else if (window.attachEvent) {
-				window.attachEvent('onmousewheel', mouseWheelListener);
+			if (this.addEventListener) {
+				for (var i = toBind.length; i;) {
+					this.addEventListener(toBind[--i], mouseWheelListener, false);
+				}
+			} else {
+				this.onmousewheel = mouseWheelListener;
 			}
 		},
 		
@@ -61,30 +75,39 @@
 
 	$.fn.extend({
 		mousewheelintention: function (fn) {
-			return fn ? this.bind('mouseWheelIntention', fn) : this.trigger('mouseWheelIntention');
+			return fn ? this.bind('mousewheelintention', fn) : this.trigger('mousewheelintention');
 		},
-		
 		unmousewheelintention: function (fn) {
-			return this.unbind('mouseWheelIntention', fn);
+			return this.unbind('mousewheelintention', fn);
 		}
 	});
 
 	var prevY = 0;
 	var lastEvents = [];
-	var lastThreeScrolls = new Array(10);
+	var lastScrolls = [];
 
 	// populate the lastEvents array with 1s
-	for (var i = 0, j = 16; i < j; i += 1) {
+	for (var i = 0; i < 16; i += 1) {
 		lastEvents.push(1);
 	}
 
+	// populate the lastScrolls array with0s
+	for (var j = 0; j < 10; j += 1) {
+		lastScrolls.push(0);
+	}
+
 	function mouseWheelListener(event) {
+		var orginalEvent = event || window.event;
 		var args = Array.prototype.slice.call(arguments, 1);
-		var y = Math.abs(event.wheelDeltaY);
+
+		event = $.event.fix(orginalEvent);
+		event.type = 'mousewheelintention';
+		
+		var y = Math.abs(event.originalEvent.wheelDeltaY);
 
 		// use detail when not wheelDelta not available like in firefox
-		if (!event.wheelDeltaY) {
-			y = Math.abs(event.detail * 3);
+		if (!event.originalEvent.wheelDeltaY) {
+			y = Math.abs(event.originalEvent.detail * 3);
 		}
 
 		// update the lastEvents array
@@ -94,14 +117,16 @@
 		var scrolling = average(lastEvents.slice(0, 5)) < average(lastEvents.slice(5));
 
 		if (scrolling) {
-			arrayremove(lastThreeScrolls, 0);
-			lastThreeScrolls.push(1);
+			arrayremove(lastScrolls, 0);
+			lastScrolls.push(1);
 		} else {
-			arrayremove(lastThreeScrolls, 0);
-			lastThreeScrolls.push(0);
+			arrayremove(lastScrolls, 0);
+			lastScrolls.push(0);
 		}
 
-		event.certainty = average(lastThreeScrolls);
+		event.certainty = average(lastScrolls);
+
+		args.unshift(event);
 
 		prevY = y;
 		return ($.event.dispatch || $.event.handle).apply(this, args);
